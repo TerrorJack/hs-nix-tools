44a652d29ae6a8759362b7e57b61163622158e71
Cheng Shao <astrohavoc@gmail.com>
2022-02-15 10:22:30 +0000

    WIP

--- nix/ci.nix
+++ nix/ci.nix
@@ -2,19 +2,26 @@
 , haskellNix ? import sources.haskell-nix { }
 , pkgs ? import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs
 , ghcs ? [ "ghc865" "ghc884" "ghc8107" "ghc902" "ghc921" ]
-, miscPkgs ? [ ]
+, miscPkgs ? [
+    "binaryen"
+    "nodejs-17_x"
+    "nodejs-16_x"
+    "nodejs-14_x"
+    "nodejs-12_x"
+    "util-linux"
+    "wabt"
+  ]
 }:
 pkgs.runCommand "hs-nix-tools-ci"
 {
-  paths =
-    map (pkg: pkgs."${pkg}") miscPkgs ++ pkgs.lib.concatMap
-      (ghc:
-        [ pkgs.haskell-nix.compiler."${ghc}" ]
+  paths = map (pkg: pkgs."${pkg}") miscPkgs ++ pkgs.lib.concatMap
+    (ghc:
+      [ pkgs.haskell-nix.compiler."${ghc}" ]
         ++ pkgs.lib.attrValues (import ./tools.nix { inherit ghc; }))
-      ghcs ++ [
-      (pkgs.haskell-nix.tool "ghc8107" "hoogle" {
-        version = "5.0.18.2";
-        index-state = pkgs.haskell-nix.internalHackageIndexState;
-      })
-    ];
+    ghcs ++ [
+    (pkgs.haskell-nix.tool "ghc8107" "hoogle" {
+      version = "5.0.18.2";
+      index-state = pkgs.haskell-nix.internalHackageIndexState;
+    })
+  ];
 } "export > $out"
--- pkgs/wasmtime/default.nix
+++ pkgs/wasmtime/default.nix
@@ -1,25 +1,25 @@
 { autoPatchelfHook, fetchzip, lib, stdenv }:
 stdenv.mkDerivation rec {
   pname = "wasmtime";
-  version = "0.33.0";
+  version = "0.34.0";
   src = fetchzip {
     x86_64-linux = {
       url =
         "https://github.com/bytecodealliance/wasmtime/releases/download/v${version}/wasmtime-v${version}-x86_64-linux.tar.xz";
       hash =
-        "sha512-EVDX2G3BP/Wp5ivtdEldlXtOYZPHkwiu29iKvKacPKr1dxJJcl5iHBuS18JJ6Kz2RRxD+ShUfOoVxh7f9NEN6Q==";
+        "sha512-nSRREqwAXvCabvCoL+BmGIU0B9k0H+BRxKLLvnZJvXyNmmwt3B2Gs3sld4f6DDUePrya54FexwtF9/F10yw+1w==";
     };
     aarch64-linux = {
       url =
         "https://github.com/bytecodealliance/wasmtime/releases/download/v${version}/wasmtime-v${version}-aarch64-linux.tar.xz";
       hash =
-        "sha512-EVnl9bCqD2Kl5tE1G42D8DNN4hiUnIOstET2gGc6saIeYsYqyuylBw6K/O2ayAU1tKL04QcnOQ2HgxmuXCWRig==";
+        "sha512-wlCFS/EjkqDterc7E/n4wYO5Gs8EdjvKFoPjXaiebWkAdLdAy97l1/isHRyM1LchVeldNwngEpfrr3hFK3We9g==";
     };
     x86_64-darwin = {
       url =
         "https://github.com/bytecodealliance/wasmtime/releases/download/v${version}/wasmtime-v${version}-x86_64-macos.tar.xz";
       hash =
-        "sha512-kynqOHfA6RcZXD2kWJ6iXBe02jO63OMDcWIyUn1h00yXDFxhUgawjAp+2nW5GTg1Qq2gNgD/5Yrr0mVgk4Ko8Q==";
+        "sha512-l/3CHFUrOz84KHWtjywuXWW8ZG1xAdyD9mq7yjnDITAyx+ir3nMwbFeYIyhX1AEc+NHYOga5GoyzDOpoX0BqCQ==";
     };
   }.${stdenv.hostPlatform.system};
   nativeBuildInputs = lib.optionals stdenv.isLinux [ autoPatchelfHook ];
